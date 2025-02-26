52af8096ca8d547fc6b1bf615bc47e0e220d2ee3
nada-03 <nadd45567@gmail.com>
2025-02-26 22:44:20 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
-
-function App() {
-  // your app code here
-  return <div>Your App</div>;
-}
-
-export default App; // This line ensures that App is a default export
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { Provider } from 'react-redux'
+import store from './store.js'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+    <App />
+    </Provider>
+  </React.StrictMode>,
+)
