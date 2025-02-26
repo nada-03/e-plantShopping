0cf38493cc4c194d5441dc41def4aa51a0ae0a60
nada-03 <nadd45567@gmail.com>
2025-02-26 05:05:29 -0500

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
@@ -10,4 +11,15 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
     </Provider>
   </React.StrictMode>,
+=======
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+>>>>>>> da1b74b (initial commit)
 )
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+<<<<<<< HEAD
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/shoppingreact",
+=======
+// https://vite.dev/config/
+export default defineConfig({
+>>>>>>> da1b74b (initial commit)
   plugins: [react()],
 })
